914fb8a13261e475b7e512eb5c137bb00b9fade1
Espezzialy Souza <espezzialy@outlook.com>
2023-11-25 14:40:01 -0300

    create files and unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
-}
+    implementation(kotlin("stdlib"))
+    testImplementation("junit:junit:4.13.2")
+    testImplementation ("org.mockito:mockito-core:5.2.0")
+    testImplementation ("org.mockito:mockito-inline:5.2.0")
 
-tasks.test {
-    useJUnitPlatform()
-}
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
